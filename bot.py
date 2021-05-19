b713543ded4c1772dcbceb021c7dae44a2162720
Elliot Voris <elliot@voris.me>
2021-05-19 12:37:08 -0500

    Taking a stab at adding some messages and importing them into the bot
    I've gotten this to work in my testing discord server, and it's looking
    to me like it's working.
    
    Please review this, I'm not well-versed in discord and/or bot programming.
    
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_permission
 from discord_slash.model import SlashCommandPermissionType
 import os
+from messages import commands
 
 client = discord.Client(intents=discord.Intents.default())
 slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
@@ -22,5 +23,11 @@ async def on_ready():
 @slash.permission(guild_id=763798356484161566,permissions=quest_roles)
 async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
     await ctx.send(f"Pong! ({client.latency*1000}ms)")
+    
+for command in commands:
+    function_name = f"_{command}"
+    @slash.slash(name=command, guild_ids=guild_ids)
+    async def function_name(ctx):
+        await ctx.send(f"{commands[ctx.command.upper()]}")
 
 client.run(os.environ.get("BOT_TOKEN"))
--- messages.py
+++ messages.py
@@ -3,5 +3,13 @@ messages.py
 
 The text replies for certain commands
 """
-
-EXAMPLE_COMMAND = "This is an example reply that will be used when a command is called..."
+commands = {
+    "FAQS": "Please try the fixes and suggestions provided in our #faqs channel. Many of the most common issues are already covered there.",
+    "ANDROID_CACHE": "If you have an android phone, and you're stuck with a Play button in the lobby building screen, please try clearing the app's cache: Go to Settings -> Apps -> Stellar Quest -> Storage & Cache -> Clear Cache. **Please do not select Clear Storage on this screen.**",
+    "SERIES_SUPPORT": "We have broken our support topic into several, series-specific channels. Please ask your question in the #series-1, #series-2, or #series-3, depending on which quest you're having trouble with. This will give everyone a head start in assisting you.",
+    "KYC": "Follow the steps here, if you're having trouble with KYC Verification: https://discord.com/channels/763798356484161566/843893111435952148/843895313143169084",
+    "LOBBY_BUILDING": "Unfortunately, the Lobby Building screen is a glorified loading animation. The stars don't actually reflect how many people are currently assigned to your lobby.",
+    "WAIT_TIME": "Currently, two lobbies begin their quest every minute. All lobbies should begin within *approximately* 30 minutes. Until then, please enjoy the opportunity to grow in patience.",
+    "SPOILERS": "**DON'T POST SPOILERS.** We're all learning together, so providing screenshots, descriptions, etc. of the quest at hand before everybody has their chance to begin gives some players an unfair advantage. Don't be that guy.",
+    "RESOURCES": "Be sure to carefully look at the resources provided in the quest. There are often some hidden clues and hints within those resources."
+}
