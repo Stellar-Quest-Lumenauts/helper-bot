9bbb010aa10352a4d1a8c2f3cc181c5ac0b8c616
Matthew Graves <matthew.graves@maptions.com>
2021-05-19 16:42:43 -0700

    Support Multiple and Individual Servers from Env Vars

--- bot.py
+++ bot.py
@@ -6,30 +6,36 @@ from discord_slash.model import SlashCommandPermissionType
 from utils import get_env_vars
 from messages import commands
 
-server_id, role_id, bot_token = get_env_vars()
+server_ids, role_id, bot_token = get_env_vars() # Get Variables from Environmental Variables
 
 client = discord.Client(intents=discord.Intents.default())
 slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
 
+quest_roles = []
 
-quest_roles = [
-	create_permission(role_id, SlashCommandPermissionType.ROLE, True), # Lumenaut | Custom Role with Permissions from env var
-	create_permission(server_id, SlashCommandPermissionType.ROLE, False),  # Everyone | everyone role ID = Server ID 
-]
+for server in server_ids: 
+
+    quest_roles += [
+        create_permission(role_id, SlashCommandPermissionType.ROLE, True), # Lumenaut | Custom Role with Permissions from env var
+        create_permission(server, SlashCommandPermissionType.ROLE, False),  # Everyone | everyone role ID = Server ID 
+    ]
+
+print(quest_roles)
 
 @client.event
 async def on_ready():
     print("Ready!")
 
-@slash.slash(name="ping", guild_ids=[server_id]) # Set Server ID | This still needs to be a list, even though there's just one server
-@slash.permission(guild_id=server_id,permissions=quest_roles)
+@slash.slash(name="ping", guild_ids=server_ids)
+@slash.permission(guild_id=server_ids[0],permissions=quest_roles) # This will use the first server in your list to define which server gets your permissions for this command
 async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
     await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
+
 for command in commands:
     function_name = f"_{command}"
-    @slash.slash(name=command, guild_ids=[server_id],
-				 description=f"{commands[command][:97]}...")
+    @slash.slash(name=command, guild_ids=server_ids,
+                description=f"{commands[command][:97]}...")
     async def function_name(ctx):
         await ctx.send(f"{commands[ctx.command.upper()]}")
 
--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import os
+import json
 
 # Environmental Variable handling for ease of testing. 
-# Anyone can pull the code and put in their own values to test locally instead of modifying static ints in code and accidentally getting checked in
 
-def get_env_vars():
+def get_env_vars() -> (list, int, str):
     if os.environ.get("SERVER_ID") is None:
         print("Missing SERVER_ID environment variable, this can be obtained by Server Settings -> Widget -> Server ID")
         exit(1)
@@ -13,12 +13,25 @@ def get_env_vars():
     elif os.environ.get("BOT_TOKEN") is None:
         print("Missing BOT_TOKEN environment variable")
         exit(1)
+
     try: 
         server_id = int(os.environ.get("SERVER_ID"))
+    except ValueError as e:
+        pass
+
+    try:
+        server_id = json.loads(os.environ.get("SERVER_ID"))
+    except Exception as e:
+        print("There was an error parsing the SERVER_ID variable, please validate that is set to either an int or an array of ints")
+        exit(1)
+
+    try:
         role_id = int(os.environ.get("LUMENAUT_ROLE_ID"))
         bot_token = os.environ.get("BOT_TOKEN")
         return server_id, role_id, bot_token
     except ValueError as e:
-        print("Please verify that both SERVER_ID and LUMENAUT_ROLE_ID are integers")
-        print("Exception Details: " + str(e))
+        print("Please validate that LUMENAUT_ROLE_ID is currently set to an integer")
+
+    except Exception as e:
+        print("An unexpected error occured, Exception Details: " + str(e))
         exit(1)
