6e83c051617fef3e4657f38abcecde4cb075fc39
Matej Plavevski <matej.plavevski+github@gmail.com>
2022-11-02 15:38:41 +0100

    Rewrite Bot without Discord.Py

--- bot.py
+++ bot.py
@@ -1,34 +1,36 @@
-import discord
-import discord_slash
-from discord_slash import SlashCommand
-from discord_slash.utils.manage_commands import create_permission
-from discord_slash.model import SlashCommandPermissionType
+from interactions import Client, Intents, Permission, PermissionType
 import os
+import sys
 from messages import commands
 
-client = discord.Client(intents=discord.Intents.default())
-slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
+BOT_TOKEN = os.environ.get("HELPER_BOT_TOKEN")
+
+if BOT_TOKEN is None:
+    print("BOT TOKEN IS MISSING")
+    sys.exit(1)
+
+client = Client(token=BOT_TOKEN)
 guild_ids = [703325111209427015, 763798356484161566] # Test Server, Stellar Quest Server
 
 quest_roles = [
-	create_permission(766768688342499390, SlashCommandPermissionType.ROLE, True), # Lumenaut
-	create_permission(763798356484161566, SlashCommandPermissionType.ROLE, False),  # Everyone
+	Permission(id=766768688342499390, type=PermissionType.ROLE, permission=True), # Lumenaut
+	Permission(id=763798356484161566, type=PermissionType.ROLE, permission=False)  # Everyone
 ]
 
 @client.event
 async def on_ready():
     print("Ready!")
 
-@slash.slash(name="ping", guild_ids=guild_ids)
-@slash.permission(guild_id=763798356484161566,permissions=quest_roles)
+@client.command(name="ping", description="Ping!", scope=guild_ids)
 async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
     await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
 for command in commands:
+    command = command.lower()
     function_name = f"_{command}"
-    @slash.slash(name=command, guild_ids=guild_ids,
-				 description=f"{commands[command][:97]}...")
+    @client.command(name=command, scope=guild_ids,
+				 description=f"{commands[command.upper()][:97]}...")
     async def function_name(ctx):
-        await ctx.send(f"{commands[ctx.command.upper()]}")
+        await ctx.send(f"{commands[command.upper()]}")
 
-client.run(os.environ.get("BOT_TOKEN"))
+client.start()
--- messages.py
+++ messages.py
@@ -12,5 +12,8 @@ commands = {
     "WAIT_TIME": "Currently, two lobbies begin their quest every minute. All lobbies should begin within *approximately* 30 minutes. Until then, please enjoy the opportunity to grow in patience.",
     "SPOILERS": "**DON'T POST SPOILERS.** We're all learning together, so providing screenshots, descriptions, etc. of the quest at hand before everybody has their chance to begin gives some players an unfair advantage. Don't be that guy.",
     "RESOURCES": "Be sure to carefully look at the resources provided in the quest. There are often some hidden clues and hints within those resources.",
-    "TAX": "We are not tax lawyers. Please don’t ask for specific tax advice, and we’ll try our best not to give it. If you do have any questions, please head over to our <#844983942473973760> channel for a list of useful information."
+    "TAX": "We are not tax lawyers. Please don’t ask for specific tax advice, and we’ll try our best not to give it. If you do have any questions, please head over to our <#844983942473973760> channel for a list of useful information.",
+    "TX_BAD_SEQUENCE": "It looks like you are attempting to submit a transaction with an invalid sequence number. In laboratory you can fix this by pressing the **Fetch Next Sequence Button** again and repeating the same steps for submitting a transaction. ",
+    "TX_BAD_AUTH": "The following error can be interpreted in 2 ways. If you are not using Multisignature, the error states that you are using the wrong Secret Key to execute a particular transaction. If you are using Multisign, you may have not used enough signers to reach the thresholds, which leads to the transaction failing.",
+    "MERGE_ACCOUNT": "This Resource might be relevant to your problem: https://youtu.be/80OhSSWan78"
 }
